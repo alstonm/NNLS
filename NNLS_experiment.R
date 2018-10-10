7c155cfddc10c9694834d184ec988a89c19dc396
sbastkowski <sarah.bastkowski@earlam.ac.uk>
2018-10-10 14:36:10 +0100

    Fixed some bugs and seperated tutorial to run functions in seperate script "tutorial.R"

--- NNLS_experiment.R
+++ NNLS_experiment.R
@@ -1,26 +1,19 @@
-#####################################################################
-## Solving a system of linear equations for a non-square [m > n]   ##
-## matrix via the use of non-negative least squares [NNLS].        ##
-## Returns the solution 'weights' and residual sum of squares.     ##
-##                                                                 ## 
-## Mark Alston,                                                    ##
-## Cell & Developmental Biology,                                   ##
+####################################################################
+## Solving a system of linear equations for a non-square          ##
+## matrix via the use of non-negative least squares [NNLS].       ##
+## Returns the solution 'weights' and residual sum of squares.    ##
+##                                                                ## 
+## Mark Alston,                                                   ##
+## Cell & Developmental Biology,                                  ##
 ## John Innes Centre, Norwich, UK.     
 ## mark.alston@jic.ac.uk  
 ## 
-## Sarah Bastkowski
-## Earlham Institute
+## Sarah Bastkowski,
+## Earlham Institute, Norwich, UK.
 ## sarah.bastkowski@earlham.ac.uk
 ##
 ##                                              
 #####################################################################
-
-
-## ========================================================================================================= ##
-## Install the R packages phyloseq, biomformat, nnls and limSolve.
-## Now load as required...  
-## ========================================================================================================= ##
-
 
 
 
@@ -34,7 +27,7 @@
 
 #Set up function that reads in files, pooles samples
 
-read_data <- function(biomfile, mappingfile, pool) {
+read_data <- function(biomfile, mappingfile, pool=NULL) {
   
   library("biomformat")
   library("phyloseq")
@@ -52,10 +45,7 @@ read_data <- function(biomfile, mappingfile, pool) {
 }
 
 
-
-
-
-# Change to samplenames 
+# Little helper to create pooling matrix
 # Need to specify how to present the pooling or add function to pull this out of mapping file
 makePool<-function(size){
   
@@ -69,10 +59,10 @@ makePool<-function(size){
 }
 
 
-#Input: OTU table and matrix that indicates which samples should be pooled, 
-#row is one pool and columns (number of samples) indicates which Samples are in this pool
+# Input: OTU table and matrix that indicates which samples should be pooled, 
+# row is one pool and columns (number of samples) indicates which Samples are in this pool
 # if entry is 0 its not in pool, if sample is 1 it is in pool.
-#single samples are treated as a pool as well, ie one row with zeros except one 1
+# single samples are treated as a pool as well, ie one row with zeros except one 1
 poolSamples<-function(phyloseqObj, pool) {
   pooledOTU=rep(0,nrow(otu_table(phyloseqObj)))
  
@@ -89,13 +79,16 @@ poolSamples<-function(phyloseqObj, pool) {
   
   return(otu_table(pooledOTU, taxa_are_rows = TRUE))
 }
+
 singles=c(1,2,3)
 mixed=c(4,5,6,7,8,9,10)
 ## ========================================================================================================= ##
-## collapse OTU table at level given as string, eg "Family", and generate matrix 'A' (signle communities)
+## collapse OTU table at <level> given as string, eg "Family", and generate matrix 'A' (single communities)
 ## and 'b' (mixed communities)
+## Input: the phyloseq object, level, vector stating which columns in the matrix (OTU) are single samples (A)
+## and which ones are mixed (b)
 ## ========================================================================================================= ##
-set_up_system_of_equations <- function(myPhyloseq, level, singles, mixed) {
+set_up_A <- function(myPhyloSeq, level, singles) {
   
   bacteria_level <- tax_glom(myPhyloSeq, taxrank=level)
   bacteria_level_df <- as.data.frame(get_taxa(otu_table(bacteria_level)))
@@ -103,90 +96,52 @@ set_up_system_of_equations <- function(myPhyloseq, level, singles, mixed) {
   bacteria_level_singleComm_df  <-  bacteria_level_df[,singles] 
   # A
   bacteria_family_matrix_A <- as.matrix(bacteria_level_singleComm_df)
-  
-  # b
-  my_bvectors <- list()
-  for (i in 1:length(mixed)) 
-  {
-    # create a name...
-    name <- paste("b_M0",i,"_bac_family_df", sep = "")
-    
-    # ...and assign something to an object with that name
-    bvectorObject <- assign(name, bacteria_level_df[,mixed[i]])
-    
-    # create a name for the b-vector
-    bvector_name <- paste("bv_M0",i,sep = "")
-    
-    # assign something to an object with that name & add the b-vector to a list 'my_bvectors'
-    my_bvectors[[i]] <- assign(bvector_name, as.matrix(bvectorObject))
-    
-    
-  }
+  return(bacteria_family_matrix_A)
 }
 
+set_up_b <- function(myPhyloSeq, level, mixed) {
+# b
+my_bvectors <- list()
+for (i in 1:length(mixed)) 
+{
+  bacteria_level <- tax_glom(myPhyloSeq, taxrank=level)
+  bacteria_level_df <- as.data.frame(get_taxa(otu_table(bacteria_level)))
+  # create a name...
+  name <- paste("b_M0",i,"_bac_family_df", sep = "")
+  
+  # ...and assign something to an object with that name
+  bvectorObject <- assign(name, bacteria_level_df[,mixed[i]])
+  
+  # create a name for the b-vector
+  bvector_name <- paste("bv_M0",i,sep = "")
+  
+  # assign something to an object with that name & add the b-vector to a list 'my_bvectors'
+  my_bvectors[[i]] <- assign(bvector_name, as.matrix(bvectorObject))
+}
 
+return(my_bvectors)
 
-
-row.names(bacteria_family_singleComm_df) <- NULL
-colnames(bacteria_family_singleComm_df) <- NULL
-
-
-## ========================================================================================================= ##
-## get matrix 'A' 
-## ========================================================================================================= ##
-
-
-
-
-
-## define which otu_table columns correspond to the MIXED communities 
-
-#mixedSamples <- c(11,12,10,19,15,16,13,14,8,9)
-mixedSamples <- c(10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30)
-mixedPooledSamples<-c(4,5,6,7,8,9,10)
-#mixedSamples <- c(10,11,12)
-mixedSamples
-no_of_mixedSamples <- length(mixedPooledSamples)
-
-
-
+}
 
 runNNLS<-function(matrix, vector){
   library("nnls")
   my_weightsList <- list()
   
-  for (i in 1:length(my_bvectors)) 
+  for (i in 1:length(vector)) 
   {
     # create a name...
     weightName <- paste("weight_M",i, sep = "")
-    weightObject <-  nnls(bacteria_family_matrix_A, my_bvectors[[i]])
-    my_weightsList[[i]] <- assign(weightName, weightObject )
+    weightObject <-  nnls(matrix,vector[[i]])
+    my_weightsList[[i]] <- assign(weightName, weightObject$x)
   }
   
   
   return(my_weightsList)
 }
 
-
-write.table(runNNLS(bacteria_family_matrix_A, my_bvectors), sep="\t", "refactored_solutionWeights.txt")
-weightSolutions=runNNLS(bacteria_family_matrix_A, my_bvectors)
-
-
-###### ALL OK UP TO THIS POINT ###########
-
-
-# need to set the names within the lists
-## name columns and rows ##
-###########################
-
-
-
 ## ========================================================================================================= ##
 ## THIRD: Plot, arrange and output the solution 'weight' values as barcharts
 ## ========================================================================================================= ##
-
-
-
 
 
 
@@ -203,7 +158,7 @@ outputBarcharts <- function(weightSolutions){
   
   for (i in 1:length(weightSolutions)) 
   {
-    trellis.objects.list[[i]] <- barchart(as.table(weightSolutions[[i]]$x),
+    trellis.objects.list[[i]] <- barchart(as.table(weightSolutions[[i]]),
                                           main=colnames(weightSolutions)[i],
                                           horizontal=FALSE, col="steelblue", ylab="Weight", aspect=1,
                                           scales=list(x=list(rot=70, labels=seed_sample_names, cex=1.1)))
@@ -224,5 +179,3 @@ outputBarcharts <- function(weightSolutions){
   return(multiPageGrobs)
 } 
 # end of 'outputBarcharts' function
-# end of 'outputBarcharts' function
-ggsave(file = "multipage_A4.pdf", outputBarcharts(weightSolutions), width = 8.27, height = 11.69, unit = "in")
