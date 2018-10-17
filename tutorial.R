8d0e3ecd9cb3c7006c2a7352ce1e30aa0394252b
sbastkowski <sarah.bastkowski@earlam.ac.uk>
2018-10-17 16:00:58 +0100

    Call initial data structure generator with pool now

--- tutorial.R
+++ tutorial.R
@@ -19,16 +19,16 @@ makePool<-function(size){
   return(pool)
 }
 # make pool
-#pool=makePool(3)
+pool=makePool(3)
 
 # read in data and create obj
 
-myObj=read_data("CSS_norm_mcf-0.001.biom","mappingFile.txt")
+myObj=read_data("CSS_norm_mcf-0.001.biom","mappingFile.txt", pool = pool)
 
-#singles=c(1,2,3)
-singles=c(1,2,3,4,5,6,7,8,9)
-#mixed=c(4,5,6,7,8,9,10)
-mixed=c(10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30)
+singles=c(1,2,3)
+#singles=c(1,2,3,4,5,6,7,8,9)
+mixed=c(4,5,6,7,8,9,10)
+#mixed=c(10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30)
 
 # Set up system of lin equations
 
